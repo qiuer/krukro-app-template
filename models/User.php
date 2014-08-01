fd00c3a143051855b7ec3a8191c70ffa24b82e6a
SerjRamone <sergey.greznov@gmail.com>
2014-08-01 16:28:26 +0400

    model fix

--- models/User.php
+++ models/User.php
@@ -27,6 +27,14 @@ class User extends ActiveRecord implements IdentityInterface
     /**
      * @inheritdoc
      */
+    public static function tableName()
+    {
+        return '{{%user}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
     public static function findIdentity($id)
     {
         return static::findOne($id);
